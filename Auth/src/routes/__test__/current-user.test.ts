97731dd59cc665072ec63fb6d8bf43b2c8632332
Mutalib0003 <mutalib.adebayo@mmgafrica.com>
2026-04-20 12:46:10 +0100

    failed pull request

--- Auth/src/routes/__test__/current-user.test.ts
+++ Auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it('responds with details about the current user', async () => {
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
-    .expect(200);
+    .expect(5000);
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
