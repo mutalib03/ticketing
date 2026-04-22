0c360a660204ca991fdfdface83e5a0129e2df64
Mutalib0003 <mutalib.adebayo@mmgafrica.com>
2026-04-22 15:42:20 +0100

    Dev

--- Payments/src/index.ts
+++ Payments/src/index.ts
@@ -8,7 +8,7 @@ import { natsWrapper } from './nats-wrapper';
 
 
 const start = async () => {
-
+ console.log("Starting up payments service......,,,,..")
   if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defined")
   if (!process.env.NATS_URL) throw new Error("NATS_URL must be defined")
   if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defined") 
--- Tickets/src/index.ts
+++ Tickets/src/index.ts
@@ -6,7 +6,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
- console.log("Starting up tickets service...")
+ console.log("Starting up tickets service.....")
   if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
   if (!process.env.MONGO_URI) throw new Error("MONGO_URL must be defined")
   if (!process.env.NATS_CLUSTER_ID) throw new Error("NATS_CLUSTER_ID must be defined")
